3d2f07a286d0b06a169f95863ae952fcd0e316db
Harhao <lvhuahao@corp.netease.com>
2023-04-07 18:19:35 +0800

    perf: 遵循单一设计原则调整

--- packages/pilot-script/bin/index.ts
+++ packages/pilot-script/bin/index.ts
@@ -1,13 +1,29 @@
 import cac from 'cac';
 import Pilot from '../scripts/pilot';
+import pkg from '../package.json';
 
 
-const cli = cac();
+const cli = cac(`${pkg.name}`);
+const pilot = new Pilot();
+
+cli
+    .command('configure [envname]', '[envname]提供部署github项目')
+    .alias('cf')
+    .action((envname: string) => {
+        pilot.execute();
+    });
+
+
+cli
+    .command('clone [project]', '[project]提供部署github项目')
+    .alias('dp')
+    .action((gitUrl: string) => {
+        pilot.execute();
+    });
 cli
     .command('deploy [project]', '[project]提供部署github项目')
     .alias('dp')
-    .action((project) => {
-        const pilot = new Pilot();
+    .action((project: string) => {
         pilot.execute();
     });
 
--- packages/pilot-script/scripts/pilot.ts
+++ packages/pilot-script/scripts/pilot.ts
@@ -2,18 +2,25 @@ import prompts from 'prompts';
 import CmdScript from './cmd';
 import { projectConfig, deployConfig } from '../config/index';
 import Log from '../scripts/utils/log';
-import path, { resolve } from 'path';
+import path from 'path';
 import fse from 'fs-extra';
 import { Client } from 'ssh2';
 import { ENVCONFIGNAME } from '../consts/index';
 
-interface IEnvConfig {
-    'address': string;
-    'serverPass': string;
-    'gitUser': string;
-    'gitPass': string;
+interface IPilotCofig {
+    address: string;
+    serverPass: string;
+    gitUser: string;
+    gitPass: string;
 }
 
+interface IProjectCofig {
+    gitUrl: string;
+    branch: string;
+    tool: string;
+    command: string;
+    dest: string
+}
 
 export interface IPilotOptions {
     deployFolder: string;
@@ -21,41 +28,67 @@ export interface IPilotOptions {
 
 export default class Pilot {
     private cmd: CmdScript;
+    // 本地配置密钥信息路径
     private configPath: string;
     private client: Client = new Client();
     private fileSftp: any;
+
     constructor() {
         this.cmd = new CmdScript({});
         this.configPath = this.getConfigPath();
     }
 
-    public async execute() {
-        Log.success('✨ 开始运行pilot脚本 ✨');
-        const hasGitEnv = this.checkGitConfig();
-        if (!hasGitEnv) {
-            const answers = await prompts(deployConfig);
-            this.createGitEnv(answers);
+    public async initConfigure() {
+        try {
+            const hasGitEnv = this.checkGitConfig();
+            let answers = null;
+            if (!hasGitEnv) {
+                answers = await prompts(deployConfig);
+                this.createGitEnv(answers);
+            } else {
+                answers = fse.readJsonSync(this.configPath);
+            }
+            return answers;
+        } catch (e) {
+            Log.error(`initConfigure  error ${e}`);
         }
-        this.startDeployJob();
     }
 
-    public async downloadRepo(gitUrl: string, data: IEnvConfig) {
+    public async initProject() {
+        try {
+            return await prompts(projectConfig);
+        } catch (e) {
+            Log.error(`initConfigure  error ${e}`);
+        }
+    }
+
+    public async execute() {
+        Log.success('✨ 开始运行pilot脚本 ✨');
+        const pilotCofig = (await this.initConfigure()) as IPilotCofig;
+        const projectConfig = await this.initProject() as IProjectCofig;
+        const localPath = await this.downloadRepo(projectConfig.gitUrl, pilotCofig);
+        console.log(localPath);
+        if (localPath) {
+            this.deployJob(localPath, projectConfig, pilotCofig);
+        }
+    }
+
+    public async downloadRepo(gitUrl: string, data: IPilotCofig) {
         if (data?.gitPass) {
             const isConfig = await this.cmd.updateGitConfigure({
                 auth: data.gitPass,
                 user: data.gitUser,
-                url: gitUrl
+                url: gitUrl,
             });
             return isConfig ? await this.cmd.cloneRepo(gitUrl) : null;
         }
         return null;
     }
 
-    public async startDeployJob() {
+    public async deployJob(localPath: string, projectConfig: IProjectCofig, pilotCofig: IPilotCofig) {
         try {
-            const { gitUrl, branch, command, tool, dest } = await prompts(projectConfig);
+            const { branch, command, tool, dest } = projectConfig;
             const data = fse.readJsonSync(this.configPath);
-            const localPath = await this.downloadRepo(gitUrl, data) as string;
             Log.success(`运行命令脚本目录是${localPath}`);
             if (localPath) {
                 const commands = command.split(' ');
@@ -63,12 +96,11 @@ export default class Pilot {
                 await this.cmd.switchToBranch(branch);
                 await this.cmd.runCmd(tool, ['install']);
                 await this.cmd.runCmd(tool, [...commands]);
-                this.uploadFileToServer(data, dest);
+                this.uploadFileToServer(pilotCofig, dest);
             }
         } catch (e) {
             Log.error(`${e}`);
         }
-
     }
 
     public getConfigPath(): string {
@@ -81,7 +113,7 @@ export default class Pilot {
         fse.writeFileSync(this.configPath, data, {
             encoding: 'utf8',
             mode: 0o644,
-            flag: 'w+'
+            flag: 'w+',
         });
     }
 
@@ -98,41 +130,40 @@ export default class Pilot {
                 Log.success(`已经存在文件目录 ${localTarget}`);
                 if (address && account && serverPass) {
                     Log.success('开始执行上传操作～');
-                    this.client.on('ready', async () => {
-                        Log.success('已经准备完毕');
-                        await this.createRemoteDirectory(`${remotePath}`);
-                        this.client.sftp(async (e, sftp) => {
-                            if(e) {
-                                Log.error(`sftp error ${e}`);
-                                return;
-                            }   
-                            this.fileSftp = sftp;
-                            this.uploadDirectory(localTarget, remotePath);
+                    this.client
+                        .on('ready', async () => {
+                            Log.success('已经准备完毕');
+                            await this.createRemoteDirectory(`${remotePath}`);
+                            this.client.sftp(async (e, sftp) => {
+                                if (e) {
+                                    Log.error(`sftp error ${e}`);
+                                    return;
+                                }
+                                this.fileSftp = sftp;
+                                this.uploadDirectory(localTarget, remotePath);
+                            });
+                        })
+                        .connect({
+                            host: address,
+                            port: 22,
+                            username: account,
+                            password: serverPass,
                         });
-                    }).connect({
-                        host: address,
-                        port: 22,
-                        username: account,
-                        password: serverPass
-                    });
-
                 }
             }
-
         } catch (e) {
             Log.error(`上传文件到服务器失败${e}`);
         }
-
     }
 
     public async createRemoteDirectory(remotePath: string): Promise<boolean> {
         return new Promise((resolve, reject) => {
             this.client.exec(`sudo mkdir ${remotePath}`, (err, stream) => {
                 if (err) {
-                    console.error(`mkdir ${remotePath} Error: ${err}`);
+                    Log.error(`mkdir ${remotePath} Error: ${err}`);
                     reject(false);
                 } else {
-                    console.log(`mkdir ${remotePath} success`);
+                    Log.success(`mkdir ${remotePath} success`);
                     resolve(true);
                 }
             });
@@ -157,16 +188,17 @@ export default class Pilot {
         return new Promise((resolve) => {
             const files = fse.readdirSync(localPath);
             files.forEach(async (file) => {
-                const stat = fse.statSync(`${localPath}/${file}`);
+                const localActualPath = `${localPath}/${file}`;
+                const remoteActualPath = `${remotePath}/${file}`;
+                const stat = fse.statSync(localActualPath);
                 if (stat.isFile()) {
-                    await this.uploadFile(`${localPath}/${file}`, `${remotePath}/${file}`);
+                    await this.uploadFile(`${localActualPath}`, `${remoteActualPath}`);
                 } else if (stat.isDirectory()) {
-                    await this.createRemoteDirectory(`${remotePath}/${file}`);
-                    this.uploadDirectory(`${localPath}/${file}`, `${remotePath}/${file}`);
+                    await this.createRemoteDirectory(`${remoteActualPath}`);
+                    await this.uploadDirectory(`${localActualPath}`, `${remoteActualPath}`);
                 }
             });
             resolve(true);
         });
     }
 }
-
